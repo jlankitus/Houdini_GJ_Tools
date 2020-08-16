fedc9f33901d440bb5282f52575484c094593eeb
Jed Lankitus <jlankitus@gmail.com>
2020-08-15 22:11:11 -0700

    Basic god mode working with 'g'

---
+++ Assets/Scripts/Character/CameraSwitcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [SerializeField] private OrbitCamera orbitCamera;
+    [SerializeField] private Camera godModeCam;
+    [SerializeField] private PlayerInput playerInput;
+
+    private bool god = false;
+
+    public void SwitchToGod()
+    {
+        orbitCamera.enabled = false;
+        godModeCam.enabled = true;
+        playerInput.enabled = false;
+    }
+
+    public void SwitchToPlayer()
+    {
+        orbitCamera.enabled = true;
+        godModeCam.enabled = false;
+        playerInput.enabled = true;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            if (!god)
+            {
+                SwitchToGod();
+                god = true;
+            }
+            else
+            {
+                SwitchToPlayer();
+                god = false;
+            }
+        }
+    }
+}
