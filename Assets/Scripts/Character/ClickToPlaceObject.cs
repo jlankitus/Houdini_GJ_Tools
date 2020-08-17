3c602ef08ad59d003eff702bbac27a89aee7c08a
Jed Lankitus <jlankitus@gmail.com>
2020-08-16 18:12:41 -0700

    WIP object placement

---
+++ Assets/Scripts/Character/ClickToPlaceObject.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToPlaceObject : MonoBehaviour
+{
+    [SerializeField] private GameObject placeableObjectPrefab;
+    [SerializeField] private GameObject currentPlaceableObject;
+    [SerializeField] private KeyCode newObjectHotKey = KeyCode.A;
+
+    private float mouseWheelRotation;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleNewObjectHotKey();
+
+        if (currentPlaceableObject != null)
+        {
+            MovePlaceableObjectToMouse();
+            RotateFromMouseWheel();
+            ReleaseIfClicked();
+        }
+    }
+
+    private void ReleaseIfClicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentPlaceableObject = null;
+        }
+    }
+
+    private void RotateFromMouseWheel()
+    {
+        mouseWheelRotation = Input.mouseScrollDelta.y;
+        Debug.LogError("wheel: "  + Input.mouseScrollDelta.y);
+        currentPlaceableObject.transform.Rotate(Vector3.up, mouseWheelRotation * 10f);
+    }
+
+    private void MovePlaceableObjectToMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            currentPlaceableObject.transform.position = hitInfo.point;
+            // Might need to get our world up, not vector3.up
+            currentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+        
+    }
+
+    private void HandleNewObjectHotKey()
+    {
+        if (Input.GetKeyDown(newObjectHotKey))
+        {
+            if (currentPlaceableObject == null)
+            {
+                currentPlaceableObject = Instantiate(placeableObjectPrefab);
+            }
+            else
+            {
+                Destroy(currentPlaceableObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/RandomObjectPlacer.cs
@@ -0,0 +1,30 @@
+﻿  using UnityEngine;
+  using System.Collections;
+  
+  public class RandomObjectPlacer : MonoBehaviour {
+  
+      public int numObjects = 10;
+      public GameObject prefabToPlace;
+      public float radius;
+  
+      void Start() 
+      {
+          Vector3 center = transform.position;
+          
+          for (int i = 0; i < numObjects; i++)
+          {
+              Vector3 pos = RandomCircle(center, radius);
+              Quaternion rot = Quaternion.FromToRotation(Vector3.forward, center-pos);
+              Instantiate(prefabToPlace, pos, rot);
+          }
+      }
+  
+      Vector3 RandomCircle ( Vector3 center ,   float radius  ){
+          float ang = Random.value * 360;
+          Vector3 pos;
+          pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+          pos.y = center.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+          pos.z = center.z;
+          return pos;
+      }
+  }
