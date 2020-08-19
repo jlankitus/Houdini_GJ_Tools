e6e6493bdce365f7b3e12c81656f93e47edee1aa
Raul M <raul.vinci87@gmail.com>
2020-08-19 02:26:09 +0200

    Revert irrelevant changes in MovingSphere

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -217,12 +217,7 @@ public class MovingSphere : MonoBehaviour {
 			Mathf.MoveTowards(currentZ, desiredVelocity.z, maxSpeedChange);
 
 		velocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
-
-        // float targetAngle = Mathf.Atan2(velocity.x, velocity.z) * Mathf.Rad2Deg;
-        //transform.forward = contactNormal - transform.local;
-        //float smoothedAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-        // transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
-
+        
         if (needsStraightened) StandUpStraight();
 	}
 
