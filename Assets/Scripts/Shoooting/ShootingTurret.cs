2c0b7eb468a2246f22f598c088f8bd30ba7ee136
Jed Lankitus <jlankitus@gmail.com>
2020-08-19 04:32:17 -0700

    turret can now track any target given to it

--- Assets/Scripts/Shoooting/ShootingTurret.cs
+++ Assets/Scripts/Shoooting/ShootingTurret.cs
@@ -6,13 +6,33 @@ public class ShootingTurret : MonoBehaviour
 {
     [SerializeField] private Transform bulletHole;
     [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform barrelRotation;
+    [SerializeField] private Transform shootTarget;
+    [SerializeField] private float strength = .5f;
+    [SerializeField] private Quaternion rotationOffset;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        rotateBarrelToTarget();
+        
+        if (Input.GetKeyDown(KeyCode.F))
         {
             Instantiate(bulletPrefab, bulletHole.transform);
         }
+    }
+
+    private void rotateBarrelToTarget()
+    {
+        var directionBetween = shootTarget.position - barrelRotation.position;
+        Debug.DrawRay(barrelRotation.position, directionBetween * 1000f);
+        var targetRotation = Quaternion.LookRotation(directionBetween);
+        targetRotation *= rotationOffset;
+        barrelRotation.rotation = targetRotation;
+        /*
+        var targetRotation = Quaternion.LookRotation (shootTarget.position - barrelRotation.position);
+        var str = Mathf.Min (strength * Time.deltaTime, 1);
+        barrelRotation.rotation = Quaternion.Lerp (transform.rotation, targetRotation, str);
+        */
     }
 }
