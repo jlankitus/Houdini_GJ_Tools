acb383897860ed898d69bb2c032af920ba82b1cc
Raul M <raul.vinci87@gmail.com>
2020-08-19 05:40:11 +0200

    Add multiple turret Placement. Minor fixes to placement.

--- Assets/Scripts/Character/ClickToPlaceObject.cs
+++ Assets/Scripts/Character/ClickToPlaceObject.cs
@@ -3,70 +3,121 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.ProBuilder.MeshOperations;
+using UnityEngine.InputSystem;
 
 public class ClickToPlaceObject : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject placeableObjectPrefab;
+    public GameObject[] placeableObjects = new GameObject[3];
+    public GameObject currentObjectToGenerate = null;
 
-    [SerializeField]
-    private KeyCode newObjectHotkey = KeyCode.A;
+    [Tooltip("The Attractor which should Pull this Object")]
+    public FauxGravityAttractor attractor;
 
-    private GameObject currentPlaceableObject;
+    //[SerializeField]
+    //private KeyCode newObjectHotkey = KeyCode.Alpha1;
+
+    private GameObject objectGhostToPlace = null;
 
     private float mouseWheelRotation;
 
-    private void Update()
+    private void Start()
     {
-        HandleNewObjectHotkey();
+        if (placeableObjects.Length > 0)
+            currentObjectToGenerate = placeableObjects[0];
+    }
 
-        if (currentPlaceableObject != null)
+    private void FixedUpdate()
+    {
+        if (objectGhostToPlace != null)
         {
             MoveCurrentObjectToMouse();
             RotateFromMouseWheel();
-            ReleaseIfClicked();
-        }
+        }   
     }
 
-    private void HandleNewObjectHotkey()
-    {
-        if (Input.GetKeyDown(newObjectHotkey))
-        {
-            if (currentPlaceableObject != null)
-            {
-                Destroy(currentPlaceableObject);
-            }
-            else
-            {
-                currentPlaceableObject = Instantiate(placeableObjectPrefab);
-            }
-        }
-    }
+
+
+    Vector3 a, b;
+
+    RaycastHit hitInfo;
 
     private void MoveCurrentObjectToMouse()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        RaycastHit hitInfo;
-        if (Physics.Raycast(ray, out hitInfo))
+        if (Physics.Raycast(ray, out hitInfo, LayerMask.GetMask("Planet")))
         {
-            currentPlaceableObject.transform.position = hitInfo.point;
-            currentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            if (hitInfo.collider != objectGhostToPlace)
+            {
+                objectGhostToPlace.transform.position = hitInfo.point;
+                objectGhostToPlace.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            }
+
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (objectGhostToPlace)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(objectGhostToPlace.transform.position, objectGhostToPlace.transform.position + a);
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(objectGhostToPlace.transform.position, objectGhostToPlace.transform.position + b);
         }
     }
 
     private void RotateFromMouseWheel()
     {
-        Debug.Log(Input.mouseScrollDelta);
         mouseWheelRotation += Input.mouseScrollDelta.y;
-        currentPlaceableObject.transform.Rotate(Vector3.up, mouseWheelRotation * 10f);
+        objectGhostToPlace.transform.Rotate(Vector3.up, mouseWheelRotation * 10f);
     }
 
-    private void ReleaseIfClicked()
+
+    /*************************************
+     * New Input System - Input Handling *
+     *************************************/
+    public void OnCreateNewTurret()
     {
-        if (Input.GetMouseButtonDown(0))
+        // Instantiate new Object
+        if (objectGhostToPlace != null)
         {
-            currentPlaceableObject = null;
+            Destroy(objectGhostToPlace);
+        }
+        else
+        {
+            objectGhostToPlace = Instantiate(currentObjectToGenerate);
+            objectGhostToPlace.GetComponent<Collider>().enabled = false;
+        }
+        // Rotate it based on Mouse Scroll
+        RotateFromMouseWheel();
+
+        // Move it to mouse position
+        MoveCurrentObjectToMouse();
+    }
+
+    public void OnConfirmBuild()
+    {
+        if (objectGhostToPlace)
+        {
+            // Make sure we have a Collider and we are targeting the Planet Layer
+            if (hitInfo.collider && hitInfo.collider.gameObject.layer == LayerMask.NameToLayer("Planet"))
+            {
+                // Make sure the Raycast hitpoint is on the Planet Surface, or close enough
+                if ((hitInfo.collider.transform.position - objectGhostToPlace.transform.position).magnitude <= hitInfo.collider.transform.localScale.magnitude * hitInfo.collider.GetComponent<SphereCollider>().radius + 0.1)
+                {
+                    // Place the turret and enable the Collider
+                    objectGhostToPlace.GetComponent<Collider>().enabled = true;
+                    // Remove the reference, so we don't move it again
+                    objectGhostToPlace = null;
+                }
+            }
         }
     }
+    
+
+    // Switch Selected Object to Place
+    public void OnChangeToObject1() { currentObjectToGenerate = placeableObjects[0]; }
+    public void OnChangeToObject2() { currentObjectToGenerate = placeableObjects[1]; }
+    public void OnChangeToObject3() { currentObjectToGenerate = placeableObjects[2]; }
 }
