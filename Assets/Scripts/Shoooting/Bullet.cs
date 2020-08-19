781fa2cffacc50bc517785f110d684012d561800
Jed Lankitus <jlankitus@gmail.com>
2020-08-19 01:37:59 -0700

    some shooting stuff!

---
+++ Assets/Scripts/Shoooting/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float speed = 2;
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * 10, ForceMode.VelocityChange);
+    }
+}
---
+++ Assets/Scripts/Shoooting/ShootingTurret.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingTurret : MonoBehaviour
+{
+    [SerializeField] private Transform bulletHole;
+    [SerializeField] private GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, bulletHole.transform);
+        }
+    }
+}
