3b328b69befbed512f96f9fc24d4b11755c006bf
Raul M <raul.vinci87@gmail.com>
2020-08-21 01:34:54 +0200

    Add Upgradeable Shield Tower - Scene Main_0
    Add Upgradeable Shield Tower

---
+++ Assets/Scripts/Character/TurretLevel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretLevel : MonoBehaviour
+{
+    public int currentLevel = 1;
+    public int maxLevel = 3;
+
+    public GameObject[] TurretModels;
+
+    private void Start()
+    {
+        // Warn the Developer that something might be missing early
+        if (TurretModels.Length != maxLevel)
+            Debug.LogError("There is a mismatch between Max Level and the current available turret Models.");
+    }
+
+    public void LevelUp()
+    {
+        // Increase current level
+        currentLevel++;
+        
+        // Make sure we don't exceed max level
+        if (currentLevel > maxLevel)
+            currentLevel = maxLevel;
+        else 
+            // Update the model only if we are not at Max level already
+            UpdateModel();
+    }
+
+    private void UpdateModel()
+    {
+        // Find and delete the Old models
+        Transform child = transform.GetChild(0);
+
+        GameObject.Destroy(child.gameObject);
+
+        // Instantiate the new Model
+        GameObject newTurretModel = Instantiate(TurretModels[currentLevel - 1], transform, false);
+    }
+}
---
+++ Assets/Scripts/Character/TurretUpgradeInteraction.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretUpgradeInteraction : MonoBehaviour
+{
+    private TurretLevel currentSelectedTurret = null;
+
+    private void Start()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateCurrentlySelectedTurret();
+    }
+    
+
+    Vector3 a, b;
+
+    RaycastHit hitInfo;
+
+    private void UpdateCurrentlySelectedTurret()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hitInfo, 500f, LayerMask.GetMask("Turret")))
+        {
+            currentSelectedTurret = hitInfo.collider.gameObject.GetComponent<TurretLevel>();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (currentSelectedTurret)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(currentSelectedTurret.transform.position, currentSelectedTurret.transform.position + a);
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(currentSelectedTurret.transform.position, currentSelectedTurret.transform.position + b);
+        }
+    }
+
+
+    /*************************************
+     * New Input System - Input Handling *
+     *************************************/
+
+
+    // Upgrade Turret Level
+    public void OnUpgradeTurret() {
+        if (currentSelectedTurret)
+        {
+            // Make sure we have a Collider and we are targeting the Planet Layer
+            if (hitInfo.collider && hitInfo.collider.gameObject.layer == LayerMask.NameToLayer("Turret"))
+            {
+                currentSelectedTurret.LevelUp();
+            }
+            
+        }
+    }
+}
