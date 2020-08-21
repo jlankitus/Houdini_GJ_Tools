6ad3ad5b0dc31fab7a70fd9fe5afc351afd5ab91
Jed Lankitus <jlankitus@gmail.com>
2020-08-21 12:40:24 -0700

    added main screen

---
+++ Assets/Scenes/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
